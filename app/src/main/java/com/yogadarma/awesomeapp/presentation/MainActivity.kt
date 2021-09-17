2353b1506a159a30de40295df76bf213b925a733
yogadarma13 <kadekyoga125@gmail.com>
2021-09-17 17:52:55 +0800

    setup navigation component configuration on main activity

---
+++ app/src/main/java/com/yogadarma/awesomeapp/presentation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.yogadarma.awesomeapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yogadarma.awesomeapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
