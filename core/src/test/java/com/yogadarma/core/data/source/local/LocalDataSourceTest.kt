c2bd70d609556f82296956255b968cb0bb9ac318
yogadarma13 <kadekyoga125@gmail.com>
2021-09-20 14:50:14 +0800

    make unit test for function getAllPhoto() LocalDataSource

---
+++ core/src/test/java/com/yogadarma/core/data/source/local/LocalDataSourceTest.kt
@@ -0,0 +1,70 @@
+package com.yogadarma.core.data.source.local
+
+import com.yogadarma.core.data.source.local.entity.PhotoEntity
+import com.yogadarma.core.data.source.local.room.AppDao
+import com.yogadarma.core.utils.RxImmediateSchedulerRule
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.subscribers.TestSubscriber
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+
+class LocalDataSourceTest {
+
+    private val dummyPhotoEntity = listOf(
+        PhotoEntity(
+            1,
+            "",
+            "",
+            "Yoga"
+        ),
+        PhotoEntity(
+            2,
+            "",
+            "",
+            "Darma"
+        )
+    )
+
+    @Rule
+    @JvmField
+    val rule: MockitoRule = MockitoJUnit.rule()
+
+
+    @Rule
+    @JvmField
+    var testSchedulerRule = RxImmediateSchedulerRule()
+
+    @Mock
+    private lateinit var appDao: AppDao
+
+    private lateinit var localDataSource: LocalDataSource
+
+    @Before
+    fun setUp() {
+        localDataSource = LocalDataSource(appDao)
+    }
+
+    @Test
+    fun getAllPhoto() {
+        Mockito.`when`(appDao.getAllPhoto()).thenReturn(Flowable.just(dummyPhotoEntity))
+        val result = localDataSource.getAllPhoto()
+
+        val testSubscriber = TestSubscriber<List<PhotoEntity>>()
+        result.subscribe(testSubscriber)
+        testSubscriber.assertComplete()
+        testSubscriber.assertNoErrors()
+
+        testSubscriber.assertValue(dummyPhotoEntity)
+
+        val actualResult = testSubscriber.values()[0]
+        assertEquals(dummyPhotoEntity.size, actualResult.size)
+        assertEquals(dummyPhotoEntity[0].id, actualResult[0].id)
+        assertEquals(dummyPhotoEntity[0].photographer, actualResult[0].photographer)
+    }
+}
