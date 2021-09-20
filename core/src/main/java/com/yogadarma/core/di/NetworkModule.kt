4a6811aa5638c69af6a425a50b5d8fce46f8b20b
yogadarma13 <kadekyoga125@gmail.com>
2021-09-20 21:55:30 +0800

    add security for OkHttpClient using Certificate Pinning

--- core/src/main/java/com/yogadarma/core/di/NetworkModule.kt
+++ core/src/main/java/com/yogadarma/core/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -19,6 +20,13 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val hostname = "api.pexels.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/hwzn8zePeQOG3c96zu4nXO6S4ct6O23bvfW+16/fy64=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .add(hostname, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+            .build()
+
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .addInterceptor { chain ->
@@ -31,6 +39,7 @@ class NetworkModule {
             }
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
